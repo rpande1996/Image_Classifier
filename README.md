f6c23a6c5fd518d01c38286e141525f5ede7bf89
Rajan Pande <panderajan1996@gmail.com>
2023-01-04 20:51:19 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,71 @@
+## Image_Classifier
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
+---
+## Overview
+
+This code classifies the image into the type of animal.
+
+## Softwares
+
+* Recommended IDE: PyCharm 2021.2
+
+## Libraries
+
+* Numpy 1.21.2
+* Matplotlib 3.5.0
+* SkLearn 1.1.3
+* PyTorch 1.12.1+cu116
+
+## Programming Languages
+
+* Python 3.8.12
+
+## License 
+
+```
+MIT License
+
+Copyright (c) 2022 Rajan Pande
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE.
+```
+
+## Demo
+
+Loss vs Epoch:
+
+![loss](output/Loss_p2.png)
+
+Accuracy vs Epoch:
+
+![acc](output/Acc_p2.png)
+
+Confusion Matrix:
+
+![confm](output/ConfMat_p2.png)
+
+
+
+## Build
+
+```
+git clone https://github.com/rpande1996/Image_Classifier
+cd Image_Classifier/src
+python main.py
+```
---
+++ src/main.py
@@ -0,0 +1,162 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from sklearn.metrics import confusion_matrix as cm
+from torch.utils.data import Dataset
+from tqdm import tqdm
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='../input/data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='../input/data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+## Let us show some of the training images, for fun.
+
+# functions to show an image
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+def plot_confusionMatrix(true, pred):
+    confusionMatrix = cm(true, pred)
+    plt.imshow(confusionMatrix, cmap='gray')
+    plt.xticks([])
+    plt.yticks([])
+    plt.title('Confusion Matrix')
+    plt.show()
+
+
+def plot_dict(d, ylab):
+    plt.plot(np.asarray(list(d.items()))[:, 1])
+    plt.xlabel('Epochs')
+    plt.ylabel(ylab)
+    plt.title(ylab + ' vs Epochs')
+    plt.show()
+
+
+# get some random training images
+
+
+# # show images
+# imshow(torchvision.utils.make_grid(images))
+# # print labels
+# print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+dataiter = iter(trainloader)
+images, labels = next(dataiter)
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if __name__ == '__main__':
+    net = Net()
+    # net = Net().to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.5)
+
+    epochs = 250
+    loss_val = {e: 0 for e in range(epochs)}
+    acc_val = {E: 0 for E in range(epochs)}
+    for epoch in tqdm(range(epochs)):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            # inputs, labels = inputs.to(device), labels.to(device)
+
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                if running_loss / 2000 >= 1:
+                    acc_ = 0.0
+                else:
+                    acc_ = 1.0 - (running_loss / 2000)
+                loss_val[epoch] = (running_loss / 2000)
+                acc_val[epoch] = acc_
+                running_loss = 0.0
+
+    print('Finished Training')
+
+    plot_dict(loss_val, 'Loss')
+    plot_dict(acc_val, 'Accuracy')
+
+    PATH = '../output/model/'
+    torch.save(net.state_dict(), PATH+'cifar_net.pth')
+    torch.save(net, PATH+'cifar_net.pt')
+
+
+    # prepare to count predictions for each class
+    correct_pred = {classname: 0 for classname in classes}
+    total_pred = {classname: 0 for classname in classes}
+
+    # again no gradients needed
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predictions = torch.max(outputs, 1)
+            # collect the correct predictions for each class
+            for label, prediction in zip(labels, predictions):
+                if label == prediction:
+                    correct_pred[classes[label]] += 1
+                total_pred[classes[label]] += 1
+
+    # print accuracy for each class
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 * float(correct_count) / total_pred[classname]
+        print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
+
+    plot_confusionMatrix(list(labels), list(predictions))
